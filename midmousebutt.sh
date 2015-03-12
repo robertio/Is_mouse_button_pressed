efc3f83d43c6458669e62d6c6714b26e1918199f
robertio <robert1968@gmail.com>
2015-03-12 10:36:29 +0100

    Create midmousebutt.sh

---
+++ midmousebutt.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# any program to start if middle mouse button pressed. 
+# this actually enter to a cifs mounted directory and plays mp3 files in it.
+cd /mnt/nas/mp3/play_p24
+mplayer *.mp3
